a4016b4ff39ea9ffd456db80e6bc973e1e673678
GabrielLangloisRahme <gabriel.langloisrahme@mail.mcgill.ca>
2019-05-11 22:53:38 -0400

    commit fix

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,11 @@
+//key.js - figure out what set of credentials to return
 
-module.exports={
-googleClientID: "708002656616-fusp44qgqmdprkfr027mv1kahank4vh3.apps.googleusercontent.com",
-googleClientSecret: "eiex_FAcdnBEJZvjAL_z_mXZ",
-mongoURI: "mongodb+srv://Gabriel:Gabe2018!!!@emaily-7d70b.mongodb.net/test?retryWrites=true",
-cookieKey: 'yo'
+//the if below will work with heroku, thus always else will run in local environment
+if (process.env.NODE_ENV ==="production") {
+    // we are in production return prod keys
+    module.export=require('./dev')
+}
+else {
+    //return dev keys
+    module.export=require('./dev');
 }
