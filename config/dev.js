58023e8abe199de629d6574e367eb539ef148837
GabrielLangloisRahme <gabriel.langloisrahme@mail.mcgill.ca>
2019-05-12 12:11:02 -0400

    s

---
+++ config/dev.js
@@ -0,0 +1,7 @@
+// this is dev files, don't commit this
+module.exports={
+    googleClientID: "708002656616-fusp44qgqmdprkfr027mv1kahank4vh3.apps.googleusercontent.com",
+    googleClientSecret: "eiex_FAcdnBEJZvjAL_z_mXZ",
+    mongoURI: "mongodb+srv://Gabriel:Gabe2018!!!@emaily-7d70b.mongodb.net/test?retryWrites=true",
+    cookieKey: "yos"
+    };
