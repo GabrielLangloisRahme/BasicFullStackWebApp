a0ef9c1460864d1b72b2da0988c5c33de240acb7
GabrielLangloisRahme <gabriel.langloisrahme@mail.mcgill.ca>
2019-05-12 11:39:44 -0400

    comment change

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,9 @@
 //key.js - figure out what set of credentials to return
 
+//yo
+
 //the if below will work with heroku, thus always else will run in local environment
-if (process.env.NODE_ENV ==="production") {
+if (process.env.NODE_ENV ==='production') {
     // we are in production return prod keys
     module.export=require('./prod');
 }
