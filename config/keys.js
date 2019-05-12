3214d92eb713f8aacea04a5bb98e9ede2c57a7b2
GabrielLangloisRahme <gabriel.langloisrahme@mail.mcgill.ca>
2019-05-11 23:27:18 -0400

    d

--- config/keys.js
+++ config/keys.js
@@ -3,7 +3,7 @@
 //the if below will work with heroku, thus always else will run in local environment
 if (process.env.NODE_ENV ==="production") {
     // we are in production return prod keys
-    module.export=require('./dev);
+    module.export=require('./dev');
 }
 else {
     //return dev keys
