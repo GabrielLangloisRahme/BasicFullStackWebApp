0529c0f55d22770731ae2f0b90d1486b9c768f6c
GabrielLangloisRahme <gabriel.langloisrahme@mail.mcgill.ca>
2019-05-12 13:40:51 -0400

    done

--- routes/authroutes.js
+++ routes/authroutes.js
@@ -9,7 +9,7 @@ const passport=require('passport')
 //this exports the following function to wherever called
 module.exports = (app) => {
 
-    app.get('/auth/google', passport.authenticate('google',{
+    app.get('/auth/google/callback', passport.authenticate('google',{
         scope:['profile','email']
     }));
     
